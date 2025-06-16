c4b84a0b5f25c82c2d11129adbe70794b5fbc0df
nottejas <padmakartejas24@gmail.com>
2025-06-16 12:24:07 +0530

    added charka-ui to use prebuilt comps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './components/Home';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
@@ -11,24 +13,31 @@ import AdminCreateEvent from './pages/AdminCreateEvent';
 import EventCard from './components/EventCard';
 import EventsList from './components/EventList';
 import MlReport from './components/MlReport';
+import ChakraEventCard from './components/chakra-comps/ChakraEventCard';
 
 function App() {
   return (
-    <Router>
-      <BasicExample />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path='/chat' element={<Chatbot />} />
-        <Route path='/qr' element={<Qr />}/>
-        <Route path='/admin-login'element={<AdminLogin />} />
-        <Route path='/admin-dashboard' element={<AdminDashboard />} />
-        <Route path='/admin/create-event' element={<AdminCreateEvent />} />
-        <Route path='/events' element={<EventsList />} />
-        <Route path='/ml-report' element={<MlReport />} />
-      </Routes>
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <BasicExample />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/chat" element={<Chatbot />} />
+          <Route path="/qr" element={<Qr />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/create-event" element={<AdminCreateEvent />} />
+          <Route path="/events" element={<ChakraEventCard />} /> // added to show harcoded events good for hackathon day maybe
+          {/* <Route path="/events" element={<EventsList />} /> // true events */}
+          <Route path="/ml-report" element={<MlReport />} />
+        </Routes>
+
+        {/* Optional Chakra component */}
+        <EventCard />
+      </Router>
+    </ChakraProvider>
   );
 }
 
